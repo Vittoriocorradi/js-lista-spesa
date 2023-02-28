fb51344c25c0da5c512f6d8aa5ecca9fe553d6cb
Vittoriocorradi <vittoriocorradi2310@gmail.com>
2023-02-28 16:13:03 +0100

    Lista con array prestabilito

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
 'use strict';
+
+const elementiLista = ['pane', 'acqua', 'uova', 'latte', 'farina'];
+const listaSpesa = document.getElementById('lista-spesa');
+let i = 0;
+
+while (i < elementiLista.length) {
+    listaSpesa.innerHTML += `<li>${elementiLista[i]}</li>`;
+    i++;
+}
