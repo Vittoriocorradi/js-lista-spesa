f75e6f7a4655292d865042e28415af27afb51c3b
Vittoriocorradi <vittoriocorradi2310@gmail.com>
2023-02-28 16:24:19 +0100

    Lista con array valori input

--- js/main.js
+++ js/main.js
@@ -1,10 +1,20 @@
 'use strict';
 
-const elementiLista = ['pane', 'acqua', 'uova', 'latte', 'farina'];
+// const elementiLista = ['pane', 'acqua', 'uova', 'latte', 'farina'];
+const elementiLista = [];
 const listaSpesa = document.getElementById('lista-spesa');
 let i = 0;
+const bottoneAggiungi = document.getElementById('bottone');
+const nuovoArticolo = document.getElementById('nuovo-articolo');
 
-while (i < elementiLista.length) {
-    listaSpesa.innerHTML += `<li>${elementiLista[i]}</li>`;
-    i++;
+// Evento per aggiungere personalmente elementi alla lista
+bottoneAggiungi.addEventListener('click',
+function() {
+    elementiLista.push(nuovoArticolo.value);
+    // Ciclo while
+    while (i < elementiLista.length) {
+        listaSpesa.innerHTML += `<li>${elementiLista[i]}</li>`;
+        i++;
+    }
 }
+)
